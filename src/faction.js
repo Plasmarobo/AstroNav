739a2a1fbbebd2467bb8f4101ffb649fd63913fc
Austen Higgins-Cassidy <plasmarobo@gmail.com>
2016-05-23 22:37:31 -0400

    Start Factions

---
+++ src/faction.js
@@ -0,0 +1,13 @@
+// faction.js
+
+function Faction() {
+  this.name = "Unnamed Faction";
+  this.homeworld = null;
+  this.hitpoints = 0;
+  this.wealth = 0;
+  this.force = 0;
+  this.cunning = 0;
+  this.faccreds = 0;
+  this.xp = 0;
+  this.tags = [];
+}
